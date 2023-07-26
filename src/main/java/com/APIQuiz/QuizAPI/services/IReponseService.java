84960017d324f91019da63d745b02cda9edd4ade
drissatogo <togo.drissa@odc.local>
2023-07-26 19:40:02 +0000

    reponse

--- src/main/java/com/APIQuiz/QuizAPI/services/IReponseService.java
+++ src/main/java/com/APIQuiz/QuizAPI/services/IReponseService.java
@@ -1,4 +1,20 @@
 package com.APIQuiz.QuizAPI.services;
 
+import com.APIQuiz.QuizAPI.entites.Reponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
 public interface IReponseService {
-}
+
+
+
+        Reponse getReponseById (Long idReponse);
+
+        Reponse creerReponse(Reponse reponse);
+
+        Reponse modifierReponse(Long id, String text);
+
+        void supprimerReponse(Long id);
+
+    }
--- src/main/java/com/APIQuiz/QuizAPI/services/ReponseServiceImpl.java
+++ src/main/java/com/APIQuiz/QuizAPI/services/ReponseServiceImpl.java
@@ -1,12 +1,38 @@
 package com.APIQuiz.QuizAPI.services;
 
-import com.APIQuiz.QuizAPI.repository.ParticipationRepository;
+import com.APIQuiz.QuizAPI.entites.Reponse;
 import com.APIQuiz.QuizAPI.repository.ReponseRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
+@Service
 public class ReponseServiceImpl implements IReponseService{
 
     private ReponseRepository reponseRepository;    //injection
 
+    @Override
+    public Reponse getReponseById(Long id) {
+        return reponseRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Reponse creerReponse(Reponse reponse) {
+        return reponseRepository.save(reponse);
+    }
+
+    @Override
+    public Reponse modifierReponse(Long idReponse, String text) {
+        Reponse reponse = reponseRepository.findById(idReponse).orElse(null);
+        if (reponse != null) {
+            reponse.setTexte(text);
+            return reponseRepository.save(reponse);
+        }
+        return null;
+    }
+
+    @Override
+    public void supprimerReponse(Long id) {
+        reponseRepository.deleteById(id);
+    }
 }
