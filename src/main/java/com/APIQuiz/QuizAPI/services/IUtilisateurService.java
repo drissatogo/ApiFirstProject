4953932011b5456137de65293e3ba92693dc95d7
sogore <sogoreibrahim135@gmail.com>
2023-07-26 19:48:34 +0000

    utilisateur en cours

--- src/main/java/com/APIQuiz/QuizAPI/services/IUtilisateurService.java
+++ src/main/java/com/APIQuiz/QuizAPI/services/IUtilisateurService.java
@@ -1,4 +1,18 @@
 package com.APIQuiz.QuizAPI.services;
 
+import com.APIQuiz.QuizAPI.entites.Utilisateur;
+
+import java.util.List;
+
 public interface IUtilisateurService {
+
+    Utilisateur inscrire(Utilisateur utilisateur);      // inscription de user
+    String connexion(String username, String password);
+    List<Utilisateur> listeUser();
+    Utilisateur afficherParId(Long id);
+    void supprimer(Long idUser);    // suppression de user
+    String modifier(Long iduser);       //  modifier
+
+
+
 }
--- src/main/java/com/APIQuiz/QuizAPI/services/UtilisateurServiceImpl.java
+++ src/main/java/com/APIQuiz/QuizAPI/services/UtilisateurServiceImpl.java
@@ -1,12 +1,53 @@
 package com.APIQuiz.QuizAPI.services;
 
+import com.APIQuiz.QuizAPI.entites.Utilisateur;
 import com.APIQuiz.QuizAPI.repository.ParticipationRepository;
 import com.APIQuiz.QuizAPI.repository.UtilisateurRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Service
+@Transactional
 @AllArgsConstructor
 public class UtilisateurServiceImpl implements IUtilisateurService{
 
     private UtilisateurRepository utilisateurRepository;    //injection
 
+    @Override
+    public Utilisateur inscrire(Utilisateur utilisateur) {
+//        Utilisateur emailUser = utilisateurRepository.findByEmail(email);
+        if (utilisateur==null /*&& emailUser.equals(email)*/){
+            throw new RuntimeException("Champs vides");
+        }else {
+            return utilisateurRepository.save(utilisateur);
+        }
+    }
+
+    @Override
+    public String connexion(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public List<Utilisateur> listeUser() {
+        return utilisateurRepository.findAll();
+    }
+
+    @Override
+    public Utilisateur afficherParId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void supprimer(Long idUser) {
+
+    }
+
+    @Override
+    public String modifier(Long iduser) {
+        return null;
+    }
 }
